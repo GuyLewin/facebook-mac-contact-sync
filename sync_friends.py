389cec42cec608296667e3bb729a0805414fb445
Guy Lewin <guy@lewin.co.il>
2019-02-08 16:38:46 +0100

    1. Save matching accounts into a bindings file 2. Support names with more/less than one space

--- sync_friends.py
+++ sync_friends.py
@@ -4,15 +4,13 @@ import os
 
 def main():
     r = csv.reader(open("All Friends.txt", "rb"), delimiter=',', quotechar='"')
-    for l in r:
-        display_name = l[1]
-        contact_pic = l[2]
+    for facebook_profile_url, display_name, contact_pic in r:
         if display_name == "None" or contact_pic == "None":
-            print "Invalid contact {}. Skipping".format(l)
+            print "Invalid contact {} ({}). Skipping".format(facebook_profile_url, display_name)
             continue
 
         contact_pic_path = os.path.join("Friend's Photos", contact_pic)
-        set_contact_pic.set_contact_pic(display_name, contact_pic_path)
+        set_contact_pic.set_contact_pic(facebook_profile_url, display_name, contact_pic_path)
 
 if __name__ == "__main__":
     main()
