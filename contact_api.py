52d01d3ec69a9632cd01aae9bfe05efbd1baad9f
Guy Lewin <guy@lewin.co.il>
2019-03-17 01:12:32 +0200

    Make cache and bindings shared between scrapings, and move functions around to make it more OOP

--- contact_api.py
+++ contact_api.py
@@ -2,16 +2,16 @@ import AddressBook
 import os
 import pickle
 
-CACHE_FILE = "cache.pkl"
-BINDINGS_FILE = "bindings.pkl"
+CACHE_FILE = os.path.join(os.path.dirname(__file__), "cache.pkl")
+BINDINGS_FILE = os.path.join(os.path.dirname(__file__), "bindings.pkl")
 
 class ContactAPI(object):
     def __init__(self):
         self.cache = ContactAPI._load_pickle(CACHE_FILE, set())
         self.bindings = ContactAPI._load_pickle(BINDINGS_FILE, dict())
 
-        address_book = AddressBook.ABAddressBook.sharedAddressBook()
-        self.people = address_book.people()
+        self.address_book = AddressBook.ABAddressBook.sharedAddressBook()
+        self.people = self.address_book.people()
 
     @staticmethod
     def _load_pickle(pickle_file, default=None):
@@ -43,21 +43,71 @@ class ContactAPI(object):
         with open(BINDINGS_FILE, "wb") as bf:
             pickle.dump(self.bindings, bf)
 
+    def _get_person_from_bindings(self, person_identifier):
+        if person_identifier in self.bindings:
+            # We have an addressbook entry linked to this Facebook profile
+            try:
+                return self._find_person_by_unique_id(self.bindings[person_identifier])
+            except ValueError:
+                print "Couldn't find address book entry {}. Removing this binding to identifier {}".format(bindings[person_identifier], person_identifier)
+                bindings.pop(person_identifier)
+                self._save_bindings()
+        return None
+
+    @staticmethod
+    def _change_address_book_name_property(person, key, value):
+        if not person.setValue_forProperty_(value, key):
+            print "Failed to set {} name".format(key)
+
+    def _change_contact_name(self, person, new_name):
+        new_name_parts = new_name.split(" ")
+        should_change_name = raw_input("Change the contact's name from {} to {}? [y/n]: ".format(person.displayName(), new_name))
+        if should_change_name.lower() in ("y", "yes"):
+            if len(new_name_parts) != 2:
+                print "Seting name parts manually because couldn't figure out division to <First Name> - <Middle Name> - <Last Name> automatically"
+                ContactAPI._change_address_book_name_property(person, AddressBook.kABFirstNameProperty, raw_input("First name: "))
+                ContactAPI._change_address_book_name_property(person, AddressBook.kABMiddleNameProperty, raw_input("Middle name: "))
+                ContactAPI._change_address_book_name_property(person, AddressBook.kABLastNameProperty, raw_input("Last name: "))
+            else:
+                ContactAPI._change_address_book_name_property(person, AddressBook.kABFirstNameProperty, new_name_parts[0])
+                ContactAPI._change_address_book_name_property(person, AddressBook.kABLastNameProperty, new_name_parts[1])
+                
+            if person.displayName() != new_name:
+                print "Changed name is different than requested. Current contact name: {}. Requested name: {}".format(person.displayName(), new_name)
+            else:
+                print "Successfully changed name"
+
+            # Flush to address book
+            self.address_book.save()
+
+    def _change_contact_picture(self, person, picture_path):
+        # Backup image data in case something goes wrong
+        image_data_backup = person.imageData()
+
+        python_image_data = open(picture_path, "rb").read()
+        objc_image_data = AddressBook.NSData.alloc().initWithBytes_length_(python_image_data, len(python_image_data))
+
+        if person.setImageData_(objc_image_data):
+            print "Success setting picture!"
+            self._add_to_cache(picture_path)
+        else:
+            print "Failed setting picture... Trying to restore"
+            if person.setImageData_(image_data_backup):
+                print "Restored successfully"
+            else:
+                print "Restore failed. Sorry"
+
+        # Flush to address book
+        # TODO: Don't save if failed setting image?
+        self.address_book.save()
+
     def set_contact_pic(self, person_identifier, contact_name, pic_path, offer_change_name=False):
         if pic_path in self.cache:
             print "Skipping {} because already set it's picture in previous run".format(contact_name)
             return
 
-        p = None
-
-        if person_identifier in self.bindings:
-            # We have an addressbook entry linked to this Facebook profile
-            try:
-                p = self._find_person_by_unique_id(bindings[person_identifier])
-            except ValueError:
-                print "Couldn't find address book entry {}. Removing this binding to identifier {}".format(bindings[person_identifier], person_identifier)
-                bindings.pop(person_identifier)
-                self._save_bindings()
+        # First try to see if this facebook profile was bound to an addressbook entry
+        p = self._get_person_from_bindings(person_identifier)
 
         if p is None:
             matching_people = self._find_matching_people_by_name(contact_name)
@@ -88,49 +138,12 @@ class ContactAPI(object):
             else:
                 p = list(matching_people)[0]
 
-            bindings[person_identifier] = p.uniqueId()
+            self.bindings[person_identifier] = p.uniqueId()
             self._save_bindings()
 
         print "Found {}".format(contact_name)
         
         if offer_change_name:
-            new_name_parts = contact_name.split(" ")
-            should_change_name = raw_input("Change the contact's name from {} to {}? [y/n]: ".format(p.displayName(), contact_name))
-            if should_change_name.lower() in ("y", "yes"):
-                if len(new_name_parts) != 2:
-                    print "Seting name parts manually because couldn't figure out division to <First Name> - <Middle Name> - <Last Name> automatically"
-                    if not p.setValue_forProperty_(raw_input("First name: "), AddressBook.kABFirstNameProperty):
-                        print "Failed to set first name"
-                    if not p.setValue_forProperty_(raw_input("Middle name: "), AddressBook.kABMiddleNameProperty):
-                        print "Failed to set middle name"
-                    if not p.setValue_forProperty_(raw_input("Last name: "), AddressBook.kABLastNameProperty):
-                        print "Failed to set last name"
-                else:
-                    if not p.setValue_forProperty_(new_name_parts[0], AddressBook.kABFirstNameProperty):
-                        print "Failed to set first name"
-                    if not p.setValue_forProperty_(new_name_parts[1], AddressBook.kABLastNameProperty):
-                        print "Failed to set last name"
-                    
-                if p.displayName() != contact_name:
-                    print "Changed name is different than Facebook name. Current contact name: {}. Facebook name: {}".format(p.displayName(), contact_name)
-                else:
-                    print "Successfully changed name"
+            self._change_contact_name(p, contact_name)
         
-        print "Backing up image data"
-        previous_image_data = p.imageData()
-
-        dd = open(pic_path, "rb").read()
-        d = AddressBook.NSData.alloc().initWithBytes_length_(dd, len(dd))
-
-        if p.setImageData_(d):
-            print "Success setting picture!"
-            self._add_to_cache(pic_path)
-            address_book.save()
-        else:
-            print "Failed setting picture... Trying to restore"
-            if p.setImageData_(previous_image_data):
-                print "Restored successfully"
-            else:
-                print "Restore failed. Sorry"
-        return
-        
+        self._change_contact_picture(p, pic_path)
